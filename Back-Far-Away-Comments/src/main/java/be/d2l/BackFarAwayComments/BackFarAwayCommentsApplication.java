9f5a9742c8de641f2623e1ffe52a00ae2f762d14
Vhalia <maxlegrelle@gmail.com>
2021-10-19 10:57:16 +0200

    add - empty comment project

---
+++ Back-Far-Away-Comments/src/main/java/be/d2l/BackFarAwayComments/BackFarAwayCommentsApplication.java
@@ -0,0 +1,13 @@
+package be.d2l.BackFarAwayComments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFarAwayCommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFarAwayCommentsApplication.class, args);
+	}
+
+}
