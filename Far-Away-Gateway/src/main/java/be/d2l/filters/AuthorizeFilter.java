5c0d1f8a1f25aa4676bf7b281bdcc613a59fd445
Vhalia <maxlegrelle@gmail.com>
2021-11-20 15:10:42 +0100

    add - gateway

---
+++ Far-Away-Gateway/src/main/java/be/d2l/filters/AuthorizeFilter.java
@@ -0,0 +1,30 @@
+package be.d2l.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthorizeFilter extends
+        AbstractGatewayFilterFactory<AuthorizeFilter.Config> {
+
+    public AuthorizeFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            //Filtre AVANT envoi de la requête ici
+            System.out.println("LA GATEWAY WAAAAAAAAAAAAAAAAAAAAAAAY ");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                //Filtre APRES envoi de la réponse ici
+            }));
+        };
+    }
+
+    public static class Config {
+
+    }
+}
