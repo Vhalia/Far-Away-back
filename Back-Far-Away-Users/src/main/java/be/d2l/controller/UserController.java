d48e61efa7ade9a05d4d1c38e0d443a16e576592
ThomasDorrekens <71339074+ThomasDorrekens@users.noreply.github.com>
2021-12-04 15:19:18 +0100

    Update UserController.java

--- Back-Far-Away-Users/src/main/java/be/d2l/controller/UserController.java
+++ Back-Far-Away-Users/src/main/java/be/d2l/controller/UserController.java
@@ -72,7 +72,12 @@ public class UserController {
     @GetMapping("/{mail}")
     public ResponseEntity getUserByMail(@PathVariable("mail") String mail){
         if(mail == null || mail.isEmpty() || mail.isBlank()) return ResponseEntity.badRequest().body("Empty body");
-        User userFound = service.getUserByMail(mail);
+        User userFound = null;
+        try {
+            userFound = service.getUserByMail(mail);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok().body(userFound);
     }
 
@@ -88,35 +93,4 @@ public class UserController {
         return ResponseEntity.ok().body(userFound);
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody User user, HttpServletResponse response){
-        if (!user.checkUserFields())
-            return ResponseEntity.badRequest().body("Missing mandatory information");
-        User createdUser = null;
-        try {
-            createdUser = service.createUser(user);
-        } catch (UserAlreadyExistsException e) {
-            return ResponseEntity.status(403).body("A user with email " + user.getMail() + " already exists");
-        }
-        String token = JWT.create().withIssuer("auth0").withClaim("user", createdUser.getId()).sign(jwtAlgorithm);
-        Cookie cookie = new Cookie("token", token);
-        cookie.setHttpOnly(true);
-        response.addCookie(cookie);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdUser.getId()).toUri();
-        return ResponseEntity.created(location).build();
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody User user, HttpServletResponse response) {
-        if (user.getMail() == null || user.getMail().isBlank() || user.getMail().isEmpty()
-            || user.getPassword() == null || user.getPassword().isEmpty() || user.getPassword().isBlank())
-            return ResponseEntity.badRequest().build();
-        User userFound = null;
-        try {
-            userFound = service.getUserByMail(mail);
-        } catch (UserNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok().body(userFound);
-    }
 }
